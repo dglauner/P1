870935f0986322351c6d17ed6206deca41909eec
David Glauner <david.glauner@gmail.com>
2014-09-17 15:06:41 -0400

    Adding all files

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<title>David Glauner</title>
+</head>
+<body>
+<div class="container">
+	<div class="jumbotron">
+		<h1 class="text-center">David Glauner</h1>
+		<h2 class="text-center">Dynamic Web Apps Portfolio</h2>
+		<div class="text-center">
+			<img src="img/dglauner.jpg" alt="My Picture" />
+		</div>
+	</div>
+    <div>
+		<h2>P2</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p>
+			<a class="btn btn-default" href="#" role="button">View GitHub &raquo;</a>&nbsp;
+			<a class="btn btn-default" href="#" role="button">View Live &raquo;</a>
+		</p>
+    </div>
+    <div>
+		<h2>P3</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p>
+			<a class="btn btn-default" href="#" role="button">View GitHub &raquo;</a>&nbsp;
+			<a class="btn btn-default" href="#" role="button">View Live &raquo;</a>
+		</p>
+    </div>
+    <div>
+		<h2>P4</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p>
+			<a class="btn btn-default" href="#" role="button">View GitHub &raquo;</a>&nbsp;
+			<a class="btn btn-default" href="#" role="button">View Live &raquo;</a>
+		</p>
+    </div>
+    <div>
+    	<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>I am an under-employed contractor currently working on various Salesforce.com projects. My programming background was mostly Client-Server with Oracle and VB. Looking to up my game in the web area.</p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>I have been working recently on Salesforce.com projects, I wish to upgrade my webskills with current standards, etc.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>At one time, I worked extensively with Visual Basic and Oracle.  VB was the GUI front end with Oracle as the back-end.  My oracle background included lots of work in stored procedures.  Currently I am a contractor working on Salesforce.com projects.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>Windows 8.1</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>Nope.</p>
+	</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="css/bootstrap.min.css"></script>
+</body>
+
+</html>
