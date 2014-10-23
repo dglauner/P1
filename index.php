5f047b51a15d6734456db115de5ffa63e760c8b5
David Glauner <david.glauner@gmail.com>
2014-10-23 11:53:37 -0400

    Added P3 Links

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
 		<h2>P3</h2>
 		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 		<p>
-			<a class="btn btn-default" href="#" role="button">View GitHub &raquo;</a>&nbsp;
-			<a class="btn btn-default" href="#" role="button">View Live &raquo;</a>
+			<a class="btn btn-default" href="https://github.com/dglauner/P3" role="button">View GitHub &raquo;</a>&nbsp;
+			<a class="btn btn-default" href="http://p3.davidglauner.me" role="button">View Live &raquo;</a>
 		</p>
     </div>
     <div>
