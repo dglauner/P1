79faf27685266125096f409635fbdab9a4c54878
David Glauner <david.glauner@gmail.com>
2014-12-17 17:11:47 -0500

    Added P4

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
 		<h2>P4</h2>
 		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
 		<p>
-			<a class="btn btn-default" href="#" role="button">View GitHub &raquo;</a>&nbsp;
-			<a class="btn btn-default" href="#" role="button">View Live &raquo;</a>
+			<a class="btn btn-default" href="https://github.com/dglauner/P4" role="button">View GitHub &raquo;</a>&nbsp;
+			<a class="btn btn-default" href="http://p4.davidglauner.me" role="button">View Live &raquo;</a>
 		</p>
     </div>
     <div>
